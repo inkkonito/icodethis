20d5617e8d48111774c962bc686175b3d7d8c3f1
inkkonito <pro.julienp@gmail.com>
2024-04-15 11:14:55 +0200

    init design the last jedi

---
+++ movie-card/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        '_grey': '#F4F7FB',
+        '_purple': '#2C2446',
+        '_blue': '#86A5D2',
+      }
+    },
+  },
+  plugins: [],
+}
+
